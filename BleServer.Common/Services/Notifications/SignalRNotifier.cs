bf9835efc7f35413e4a325c3533815485705d955
Saturn Technologies <info@saturn72.com>
2018-10-23 12:38:06 +0300

    add serilog for console logging

--- BleServer.Common/Services/Notifications/SignalRNotifier.cs
+++ BleServer.Common/Services/Notifications/SignalRNotifier.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using ServiceStack.Text;
 
 namespace BleServer.Common.Services.Notifications
@@ -8,15 +9,21 @@ namespace BleServer.Common.Services.Notifications
     {
 
         private readonly IHubContext<MessageHub> _messaggeHub;
+        private readonly ILogger _logger;
 
-        public SignalRNotifier(IHubContext<MessageHub> messageHub)
+        public SignalRNotifier(IHubContext<MessageHub> messageHub, ILogger<MessageHub> logger)
         {
             _messaggeHub = messageHub;
+            _logger = logger;
         }
 
         public Task Push(string key, object notification)
         {
-            return _messaggeHub.Clients.All.SendAsync(key, JsonSerializer.SerializeToString(notification));
+            var body = JsonSerializer.SerializeToString(notification);
+            var notifyTask = _messaggeHub.Clients.All.SendAsync(key, body);
+            _logger.LogInformation($"Send notification to clients: key=\'{key}\' body=\'{body}\'");
+
+            return notifyTask;
         }
     }
 
--- BleServer.WebApi/Program.cs
+++ BleServer.WebApi/Program.cs
@@ -1,18 +1,43 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace BleServer.WebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                BuildWebHost(args).Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return -666;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseSerilog()
                 .Build();
     }
 }
